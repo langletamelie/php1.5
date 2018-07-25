8ba34475dae6cf00c6fecd9faebc616d11ddf856
langletamelie <amelie.langlet@hotmail.com>
2018-07-25 19:04:36 +0200

    php1.5

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP 1.5</title>
+  </head>
+  <body>
+<p>
+<?php
+$no_value = null;
+echo $no_value;
+?>
+</p>
+<p>
+  <?php
+$no_value = 25;
+echo $no_value;
+ ?>
+</p>
+  </body>
+</html>
